e0343ea81392025c03d4e1dd549710eff68dbe88
lawlietl4 <nroberson7712@gmail.com>
2021-10-11 16:08:42 -0600

    initial commit

---
+++ capstone-api/src/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'John White',
+    },
+    2: {
+        id: '2',
+        username: 'Daeynyrs',
+    },
+};
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'join me',
+        userId: '2',
+    },
+};
+
+export default {
+    users,
+    messages,
+};
