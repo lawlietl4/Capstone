1f3904a7f89a1131d5b0e717df79c4ce7139f2be
Nicholas Roberson <nroberson7712@gmail.com>
2021-10-13 20:14:07 -0600

    attempting to add dynamic nav bar

--- capstone-app/src/app/app-routing.module.ts
+++ capstone-app/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
+//Angular Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Personal Imports
 import { MainWindowComponent } from './main-window/main-window.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TicketViewerComponent } from './ticketviewer/ticketviewer.component';
 
 const routes: Routes = [
   { path: '', component: MainWindowComponent, children:
-  [
-    {
-      path: 'ticket-viewer', component: TicketViewerComponent
-    }
-  ] 
+    [
+      {
+        path: 'ticket-viewer', component: TicketViewerComponent
+      }
+    ] 
   },
   { path: 'index', redirectTo: ''},
   { path: '404', component: PageNotFoundComponent},
--- capstone-app/src/app/app.component.ts
+++ capstone-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'capstone-app';
-}
+  constructor(pages: AppRoutingModule){
+    pages
+  };
+  
+};
