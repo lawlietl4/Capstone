8329b37a9e5faad1bd1e1a89de32021f38a9bf51
Nicholas Roberson <nroberson7712@gmail.com>
2021-10-15 01:22:01 -0600

    added guards and changed some routing

--- capstone-app/src/app/app-routing.module.ts
+++ capstone-app/src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 //Angular Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//Personal Imports
-import { MainWindowComponent } from './main-window/main-window.component';
+//Page Imports
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TicketViewerComponent } from './ticketviewer/ticketviewer.component';
 import { LoginComponent } from './login/login.component';
 import { StudentAdderComponent } from './student-adder/student-adder.component';
+import { AuthorizationGuard } from './_guards/authorization.guard';
 
 const routes: Routes = [
-  { path: '', component: MainWindowComponent },
-  { path: 'ticket-viewer', component: TicketViewerComponent },
-  { path: '404not-found', component: PageNotFoundComponent },
+  { path: 'ticket-viewer', component: TicketViewerComponent, canActivate: [AuthorizationGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'student-adder', component: StudentAdderComponent},
-  { path: '**', redirectTo: 'ticket-viewer', pathMatch: 'full' },
+  { path: '', redirectTo: '/ticket-viewer', pathMatch:'full' },
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
