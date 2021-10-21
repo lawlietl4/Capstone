6fa7a2f6eb5c2a8e091543ee1e0a34acbf09412d
lawlietl4 <nroberson7712@gmail.com>
2021-10-21 13:32:12 -0600

    added environement variables back since they work now

--- capstone-api/src/app/index.js
+++ capstone-api/src/app/index.js
@@ -1,29 +1,25 @@
-// require('dotenv').config({
-//     path: '/home/nick/Documents/Capstone/capstone-api/.env',
-// });
+require('dotenv').config();
 const { Pool } = require('pg');
 const express = require('express');
-const { json } = require('express');
 const app = express();
-const details = {
-    user: 'postgres',
-    host: 'localhost',
-    password: 'Ikeamonkey1',
-    db_port: '5432',
-    database: 'testdata',
-    port: '3000'
-};
+// const details = {
+//     user: 'postgres',
+//     host: 'localhost',
+//     password: 'Ikeamonkey1',
+//     db_port: '5432',
+//     database: 'testdata',
+//     port: '3000'
+// };
 
 // console.log(details);
-
 const pool = new Pool({
-    user: details.user,
-    host: details.host,
-    password: details.password,
-    port: details.db_port,
-    database: details.database,
+    user: process.env.user,
+    host: process.env.host,
+    password: process.env.password,
+    port: process.env.db_port,
+    database: process.env.database,
 });
-// console.log(pool);
+console.log(pool);
 const output = pool.query('SELECT * FROM users AS output', (err,res)=>{
     if(err){
         console.log(err);
@@ -54,6 +50,6 @@ app.post('/', function (req, res) {
     res.send('hello there');
 });
 
-app.listen(details.port || 3000, () => {
-    console.log(`Express listening on ${details.port}`);
+app.listen(process.env.port || 3000, () => {
+    console.log(`Express listening on ${process.env.port}`);
 });
